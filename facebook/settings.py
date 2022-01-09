5236a71d5ece74bab6cfbc9d9474f2fe3c33eafb
Prem-Vk <premvk976@gmail.com>
2022-01-09 17:48:07 +0530

    UPdated settings

--- facebook/settings.py
+++ facebook/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # 1
 # DEBUG = True
-DEBUG = True
+DEBUG = False
 
 # 2
 # ALLOWED_HOSTS = []
@@ -131,12 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_heroku.settings(locals())
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
